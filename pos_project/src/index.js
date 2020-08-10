266c8677343046306ee7c3fa661e444ab8cfa1a5
Shabaz K Mullalkar <smullalkar@gmail.com>
2020-08-10 23:50:53 +0530

    changed to has router

--- pos_project/src/index.js
+++ pos_project/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
 import { store } from './Redux/Store'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
